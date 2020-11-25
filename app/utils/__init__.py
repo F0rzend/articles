8e11ab8f31d69e2713c91ba998cfba30c0dc43a5
0Kit <kostyajog@mail.ru>
2020-11-25 21:51:36 +0300

    Remove Broadcaster

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,3 @@
-from .broadcaster import Broadcast
 from .default_commands import setup_default_commands
 from .logger import setup_logger
 from .notify_admins import notify_admins
@@ -6,6 +5,5 @@ from .notify_admins import notify_admins
 __all__ = [
     "setup_logger",
     "setup_default_commands",
-    "Broadcast",
     "notify_admins",
 ]
