ea839ebf4988c735ed0c1eab013bfd14ad4b7a30
0Kit <kostyajog@mail.ru>
2020-11-27 19:14:50 +0300

    setup logger before running bot

--- app/__main__.py
+++ app/__main__.py
@@ -9,12 +9,12 @@ from app import middlewares, filters, handlers
 
 
 async def on_startup(dispatcher: Dispatcher):
-    await utils.setup_logger("INFO", ["sqlalchemy.engine", "aiogram.bot.api"])
     await utils.setup_default_commands(dispatcher)
     await utils.notify_admins(config.SUPERUSER_ID)
 
 
 if __name__ == '__main__':
+    utils.setup_logger("INFO", ["sqlalchemy.engine", "aiogram.bot.api"])
     executor.start_polling(
         dp, on_startup=on_startup, skip_updates=config.SKIP_UPDATES
     )
