fc32791f293804c3e59f422e0bbcd5bcc38ab070
0Kit <kostyajog@mail.ru>
2020-11-28 15:41:43 +0300

    edit handlers import

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,6 +1,6 @@
 from loguru import logger
 
-from .errors import *
-from .private import *
+from .errors import retry_after
+from .private import start
 
 logger.info("Handlers are successfully configured")
