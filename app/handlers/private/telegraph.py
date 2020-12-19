4c427955c50fe0002c9fd8a6a201eb189f111b14
0Kit <kostyajog@mail.ru>
2020-12-19 16:49:09 +0300

    work without downloading

--- app/handlers/private/telegraph.py
+++ app/handlers/private/telegraph.py
@@ -3,24 +3,22 @@ from aiogram import types
 
 from app.loader import dp
 
-from tempfile import NamedTemporaryFile
+from io import BytesIO
 
 
 @dp.message_handler(content_types=types.ContentTypes.PHOTO)
 async def photo_handler(msg: types.Message):
     photo = msg.photo[-1]
-    with NamedTemporaryFile(mode='wb', prefix='file_', suffix='.jpg') as file:
-        await photo.download(file.name)
-        with open(file.name, 'rb') as f:
-            form = aiohttp.FormData()
-            form.add_field(
-                name='file',
-                value=f,
-            )
-            request_answer = await dp.bot.session.post(
-                'https://telegra.ph/upload',
-                data=form,
-            )
+    with await photo.download(BytesIO()) as file:
+        form = aiohttp.FormData()
+        form.add_field(
+            name='file',
+            value=file,
+        )
+        request_answer = await dp.bot.session.post(
+            'https://telegra.ph/upload',
+            data=form,
+        )
 
     img_src = await request_answer.json()
     link = 'http://telegra.ph/' + img_src[0]["src"]
