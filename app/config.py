b8fad1474c5216d82acfdf1d0ee3d3933697ccf6
0Kit <kostyajog@mail.ru>
2020-12-19 15:57:36 +0300

    using tempfile

--- app/config.py
+++ app/config.py
@@ -9,6 +9,5 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")
 SKIP_UPDATES = env.bool("SKIP_UPDATES", False)
 WORK_PATH: Path = Path(__file__).parent.parent
-DATA_PATH: Path = WORK_PATH.joinpath('data')
 
 SUPERUSER_IDS = env.list("SUPERUSER_IDS")
--- app/handlers/private/telegraph.py
+++ app/handlers/private/telegraph.py
@@ -1,26 +1,26 @@
 import aiohttp
 from aiogram import types
 
-from app import config
 from app.loader import dp
 
-from os.path import basename
+from tempfile import NamedTemporaryFile
 
 
 @dp.message_handler(content_types=types.ContentTypes.PHOTO)
 async def photo_handler(msg: types.Message):
-    file = await msg.photo[-1].download(config.DATA_PATH)
-    with open(file.name, 'rb') as f:
-        form = aiohttp.FormData()
-        form.add_field(
-            name='file',
-            value=f,
-            filename=basename(f.name)
-        )
-        request_answer = await dp.bot.session.post(
-            'https://telegra.ph/upload',
-            data=form,
-        )
+    photo = msg.photo[-1]
+    with NamedTemporaryFile(mode='wb', prefix='file_', suffix='.jpg') as file:
+        await photo.download(file.name)
+        with open(file.name, 'rb') as f:
+            form = aiohttp.FormData()
+            form.add_field(
+                name='file',
+                value=f,
+            )
+            request_answer = await dp.bot.session.post(
+                'https://telegra.ph/upload',
+                data=form,
+            )
 
     img_src = await request_answer.json()
     link = 'http://telegra.ph/' + img_src[0]["src"]
