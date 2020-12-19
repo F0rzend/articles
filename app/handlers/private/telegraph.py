9e52114662aa4e85345021ffc2c9c17fcf204444
0Kit <kostyajog@mail.ru>
2020-12-19 18:47:44 +0300

    upload_photo bot status

--- app/handlers/private/telegraph.py
+++ app/handlers/private/telegraph.py
@@ -7,7 +7,11 @@ from app.utils import low_photo_url, photo_url
 @dp.message_handler(content_types=types.ContentTypes.PHOTO)
 async def photo_handler(msg: types.Message):
     photo = msg.photo[-1]
+
+    await msg.bot.send_chat_action(msg.chat.id, 'upload_photo')
     link = await low_photo_url(photo)
     await msg.answer(link)
+
+    await msg.bot.send_chat_action(msg.chat.id, 'upload_photo')
     link = await photo_url(photo)
     await msg.answer(link)
