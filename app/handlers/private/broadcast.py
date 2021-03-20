4ef24ee740074776b64509f4ede3d232096fdf43
F0rzend <kostyajog@mail.ru>
2021-03-20 18:03:38 +0300

    Broadcaster examples

--- app/handlers/private/broadcast.py
+++ app/handlers/private/broadcast.py
@@ -1,3 +1,6 @@
+import logging
+
+from aiogram.contrib.fsm_storage.memory import MemoryStorage, BaseStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 
@@ -13,11 +16,26 @@ async def broadcast_command_handler(msg: Message, state: FSMContext):
     await state.set_state('broadcast_text')
 
 
+async def get_users(storage: BaseStorage):
+    """
+    Функция, возвращающая список айдишников пользователей,
+    которые будут получать сообщения в результате рассылки
+
+    Замените её на свою функцию, достающую айди из базы данных
+    """
+    if isinstance(storage, MemoryStorage):
+        users = list(storage.data.keys())  # Если в качестве storage используется ОЗУ, достаём айдишники из неё
+    else:
+        users = SUPERUSER_IDS  # В противном случае рассылку делаем по админам
+        logging.warning('Рассылка по суперпользователям')
+    return users
+
+
 async def start_broadcast(msg: Message, state: FSMContext):
     """
-    Обработчик, начинающий рассылку с введённым пользователем текстом
-    """
+        Обработчик, начинающий рассылку с введённым пользователем текстом
+        """
     await state.finish()
-    users = SUPERUSER_IDS * 20  # Список айдишников, можете доставать из базы данных или как вам удобнее.
-    # Я буду просто отправлять себе 20 сообщений для примера
+    storage = state.storage
+    users = await get_users(storage)  # Список айдишников, можете доставать из базы данных или как вам удобнее.
     await CopyBroadcast(users, msg).run()
